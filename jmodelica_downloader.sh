f3145af49994f994f8eb212b47a4c48f21617666
aviseknaug <aviseknaug1993@gmail.com>
2020-07-19 22:56:50 -0500

    jmodelica downloader from my grive

---
+++ jmodelica_downloader.sh
@@ -0,0 +1,6 @@
+fileId=10Nhb66t4zJ98MIJchWfsxBcUlxaQ5R3x
+fileName=jmodelica.zip
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
+code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName}
+
